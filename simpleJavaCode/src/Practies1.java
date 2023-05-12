7ba66487eb27d41dd2c735586fcdce8f3615d36f
muralidharsurya <smuralidar4ee@gmail.com>
2023-05-12 19:05:25 +0530

    sample java

---
+++ simpleJavaCode/src/Practies1.java
@@ -0,0 +1,13 @@
+import java.net.SocketOption;
+import java.util.Scanner;
+
+public class Practies1 {
+    public static void main(String[] args) {
+
+        int a= 7;
+        if (a%2==1)
+            System.out.println("it is prime number");
+        else
+            System.out.println("it is not prime number");
+    }
+}
